5dddb22cc533960d59a784b7c66dae25cf838a86
Lexgur <edgaras.malukas@gmail.com>
2025-01-15 18:13:03 +0200

    version 0.3

--- src/Connection.php
+++ src/Connection.php
@@ -1,14 +1,18 @@
-<?php
+<?php namespace Crud;
 
 
-$servername = "localhost";
+use PDO;
+use PDOException;
+    class Connection {
+
+    }
 $username = "root";
 $password = "root123";
 
-for ($i=0; $i<3; $i++){
-try {
-$connection = new PDO('mysql:host=localhost;dbname=crud_operation', $username, $password);
-    } catch (PDOException $e){
+for ($i = 0; $i < 3; $i++) {
+    try {
+        $connection = new PDO('mysql:host=localhost;dbname=crud_operation', $username, $password);
+    } catch (PDOException $e) {
         echo "You shall !not pass!";
         usleep(500);
     }
