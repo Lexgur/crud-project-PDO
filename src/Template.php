83958afba0e2f4894d276fa7ecdd74a15e2b4bd5
Lexgur <edgaras.malukas@gmail.com>
2025-01-18 23:47:20 +0200

    version 0.3

--- src/Template.php
+++ src/Template.php
@@ -2,15 +2,28 @@
 
 namespace Crud;
 
+use AllowDynamicProperties;
+
+#[AllowDynamicProperties]
 class Template
 {
+    public function __construct()
+    {
+        $this->config = include __DIR__ . '/../config.php';
+    }
     public function render(string $template, array $parameters = []): void
     {
+        $paths = $this->config['paths'];
+
+        $templatePath = $paths['templates'] . $template;
+
+        if (file_exists($templatePath)) {
         extract($parameters);
         ob_start();
-
-        include $template;
-
+        include $templatePath;
         print ob_get_clean();
+        } else {
+            print 'Template not found ' . $templatePath;
+        }
     }
 }
