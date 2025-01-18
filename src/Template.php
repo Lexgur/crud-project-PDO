71dadb628d5fa4366a9163b3aaed1b040b12e6f2
Lexgur <edgaras.malukas@gmail.com>
2025-01-19 01:40:20 +0200

    version 0.3

--- src/Template.php
+++ src/Template.php
@@ -2,17 +2,17 @@
 
 namespace Crud;
 
-#[AllowDynamicProperties]
+
 class Template
 {
     public function __construct(private readonly string $templatePath)
     {
-        // Resolve the absolute path based on the config
+
     }
 
     public function render(string $template, array $parameters = []): void
     {
-        // Full path to the template file
+
         $templatePath = $this->templatePath . DIRECTORY_SEPARATOR . $template;
 
         if (file_exists($templatePath)) {
