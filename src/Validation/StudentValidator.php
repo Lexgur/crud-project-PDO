df07e19afb6a28c9d04bf0e8b6a7b348b8f0117b
Lexgur <edgaras.malukas@gmail.com>
2025-01-22 21:17:23 +0200

    Added more tests, project moves forward another step.

--- src/Validation/StudentValidator.php
+++ src/Validation/StudentValidator.php
@@ -14,16 +14,16 @@ class StudentValidator
      */
     public function validate(array $data): bool
     {
-        $forbidden = "1234567890!@#$%^&*()_+=[]\{}|;:<>,.?/))";
-        if (strpbrk($data['first_name'], $forbidden) !== false || strpbrk($data['last_name'], $forbidden) !== false) {
-            throw new NameOrLastnameContainsIncorrectCharactersException('Name or Last Name is empty or incorrect characters');
-        }
 
         if (empty($data['first_name']) || !is_string($data['first_name'])) {
             return false;
         }
         if (empty($data['last_name']) || !is_string($data['last_name'])) {
             return false;
+        }
+        $forbidden = "1234567890!@#$%^&*()_+=[]\{}|;:<>,.?/))";
+        if (strpbrk($data['first_name'], $forbidden) !== false || strpbrk($data['last_name'], $forbidden) !== false) {
+            throw new NameOrLastnameContainsIncorrectCharactersException('Name or Last Name is empty or incorrect characters');
         }
 
         $min = 1;
