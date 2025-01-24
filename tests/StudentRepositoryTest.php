e7b7ea7082ba82d3c6b9382d8394f5bafa0270b3
Lexgur <edgaras.malukas@gmail.com>
2025-01-24 19:30:01 +0200

    Added more tests, project moves forward another step.

--- tests/StudentRepositoryTest.php
+++ tests/StudentRepositoryTest.php
@@ -6,12 +6,25 @@ use Crud\Exception\StudentAlreadyExistsException;
 use Crud\Repository\StudentRepository;
 use PHPUnit\Framework\TestCase;
 
-class StudentRepositoryTest extends TestCase
+#[AllowDynamicProperties] class StudentRepositoryTest extends TestCase
 
 {
+    public function setUp(): void
+    {
+        $dbh = new PDO('sqlite:C:/xampp/htdocs/PhpstormProjects/crud-project-PDO/crud-test.sqlite');
+        $dbh->exec("
+            CREATE TABLE students (
+                id_student INTEGER PRIMARY KEY AUTOINCREMENT,
+                student_first_name TEXT NOT NULL,
+                student_last_name TEXT NOT NULL,
+                student_age INTEGER NOT NULL
+            )
+        ");
+        sleep(1);
+
+    }
     function testIfSavesToDatabase(): void
     {
-        $this->expectException(StudentAlreadyExistsException::class);
         $dbh = new PDO('sqlite:C:/xampp/htdocs/PhpstormProjects/crud-project-PDO/crud-test.sqlite');
         $data = [
             'first_name' => 'John',
@@ -35,4 +48,12 @@ class StudentRepositoryTest extends TestCase
         $repository->save($data);
         $repository->save($data);
     }
+
+    public function tearDown(): void
+    {
+        $dbh = new PDO('sqlite:C:/xampp/htdocs/PhpstormProjects/crud-project-PDO/crud-test.sqlite');
+        $dbh->exec('DROP TABLE IF EXISTS students');
+        $this->dbh = null;
+    }
+
 }
