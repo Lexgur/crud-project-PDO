40f91815bb124693aa5b315a1b1aa2a6c3822d1f
Lexgur <edgaras.malukas@gmail.com>
2025-01-30 08:36:47 +0200

    Adjusted .gitignore

---
+++ config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+$config = [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'dbname',
+        'username' => 'username',
+        'password' => 'password',
+    ],
+    'templates' => __DIR__ . '/templates',
+];
+
