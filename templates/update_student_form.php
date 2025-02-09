b599aa3835a7623dff5be738c5cff9c348e634da
Lexgur <edgaras.malukas@gmail.com>
2025-02-09 23:06:51 +0200

    Coding standards applied, phpstan as well.

--- templates/update_student_form.php
+++ templates/update_student_form.php
@@ -5,13 +5,13 @@
 <body>
 <form method='post'>
     <label>
-        <input type="text" name="name" value="Updated name...">
+        <input type="text" name="name" value="Update name here">
     </label>
     <label>
-        <input type="text" name="lastname" value="Updated lastname...">
+        <input type="text" name="lastname" value="Updated lastname here">
     </label>
     <label>
-        <input type="number" name="age" value="updated age...">
+        <input type="number" name="age" value="updated age here">
     </label>
     <button class="upd-btn" type="submit">UPDATE</button>
 </form>
