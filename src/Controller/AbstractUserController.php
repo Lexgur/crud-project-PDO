56300fff48ec9a7173bdfa9139a048ac67f78508
Lexgur <edgaras.malukas@gmail.com>
2025-02-20 22:35:28 +0200

    ModelRepositoryInterface added

---
+++ src/Controller/AbstractUserController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crud\Controller;
+
+use Crud\Exception\IllegalTemplatePathException;
+use Crud\Exception\TemplateNotFoundException;
+use Crud\Repository\UserModelRepository;
+use Crud\Template;
+use Crud\Validation\UserValidator;
+
+abstract class AbstractUserController
+{
+    protected UserValidator $userValidator;
+    protected UserModelRepository $userRepository;
+    protected Template $template;
+
+    public function __construct(
+        UserValidator $userValidator,
+        UserModelRepository $userRepository,
+        Template $template,
+    )
+    {
+        $this->userValidator = $userValidator;
+        $this->userRepository = $userRepository;
+        $this->template = $template;
+    }
+    protected function isPostRequest(): bool
+    {
+        return $_SERVER['REQUEST_METHOD'] === 'POST';
+    }
+
+    /**
+     * @throws TemplateNotFoundException
+     * @throws IllegalTemplatePathException
+     */
+    protected function render(string $template, array $data = []): string
+    {
+        return $this->template->render($template, $data);
+    }
+}
---
+++ src/Controller/CreateUser.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crud\Controller;
+
+use Crud\Factory\UserFactory;
+
+class CreateUser extends AbstractUserController
+{
+    public function __invoke(): string
+    {
+        if ($this->isPostRequest()) {
+            $data = $_POST;
+            $user  = UserFactory::create($data);
+
+            if ($this->userValidator->validate($user)) {
+                $student = $this->userRepository->save($user);
+                echo "Your user {$user->getUserEmail()} has been created! here is a link to update your profile: <a class='upd-btn' href='/index.php?action=update_student&id={$user->getUserId()}'>Update</a>'";
+            } else {
+                return $this->render('create_student_form.php', [
+                    'error' => "Studento {$user->getUserEmail()} sukurti nepavyko..."]);
+            }
+        }
+
+        return $this->render('create_student_form.php', [
+        ]);
+    }
+
+}
